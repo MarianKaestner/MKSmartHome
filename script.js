1ab0a21773de105457d2cbe1c65b3f0e309267e4
Marian Kästner <mariankaestner2005@gmail.com>
2023-12-22 10:31:44 +0100

    Changed js so it's pure jQuery

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
-function addElement(){
-    var node = document.createElement("div");
-    node.attr("class", "widget");
+$("#add-device").click(function(){
+    var node = $("<div></div>");
+    node.addClass("widget");
     node.html("<h2>Smart Device</h2>");
     $("#add-device").before(node);
-}
+});
+
+
+
