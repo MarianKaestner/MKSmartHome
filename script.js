d8f869a4aba6c1236b9df0706acce2265d1cdd6f
Marian Kästner <mariankaestner2005@gmail.com>
2023-12-22 10:58:31 +0100

    Changed flexwrap to wrap and added remove function with onclick attribute assignment in add device funtion

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 
+//Adds new device
 $("#add-device").click(function(){
     var node = $("<div></div>");
     node.addClass("widget");
+    node.attr("onclick", "removeDevice(this)");
     node.html("<h2>Smart Device</h2>");
     $("#add-device").before(node);
 });
+
+//Removes device
+function removeDevice(e){
+    e.remove();
+}
 
 
 
