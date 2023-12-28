9b0db53ba979259c47e630981837248a5dfc92a4
Marian Kästner <mariankaestner2005@gmail.com>
2023-12-28 08:15:17 +0100

    Added body parser and made express deliver style and script file

--- backend.js
+++ backend.js
@@ -1,7 +1,11 @@
 let path = require("path");
 let express = require("express");
+let parser = require("body-parser");
 app = express();
 let port = 8000;
+
+app.use(parser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     console.log(path.join(__dirname, "/public/index.html"));
