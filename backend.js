ea9c80dd67f6bf01be742959476287c805b75d95
Marian Kästner <mariankaestner2005@gmail.com>
2023-12-29 18:40:13 +0100

    Made remove feature serverside

--- backend.js
+++ backend.js
@@ -3,15 +3,34 @@ let express = require("express");
 let parser = require("body-parser");
 app = express();
 let port = 8000;
+app.locals.deviceCounter = 0;
+app.locals.deviceList = [];
+
+function Device(pName, pAddress){
+    this.name = pName;
+    this.address = pAdress;
+}
 
 app.use(parser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    console.log(path.join(__dirname, "/public/index.html"));
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.render("index.ejs");
 });
+
+app.get("/newdevice"), (req, res) => {
+    res.render("deviceform.ejs");
+}
+
+app.post("/submit", (req, res) => {
+    res.redirect("http://localhost:8000/newdevice");
+});
+
+app.post("/create", (req, res) => {
+    app.locals.deviceList.push(new Device(req.body.name, req.body.ipaddress));
+    res.redirect("http://localhost:8000");
+})
 
 app.listen(port, () => {
     console.log("Listening on port " + port);
-})
+});
